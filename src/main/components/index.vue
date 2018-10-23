358d3f8184c27fae72186de17cf3d22085b83a95
magee <magee@machaodeMacBook-Pro.local>
2018-10-23 14:03:13 +0800

    初始化

---
+++ src/main/components/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <img src="../assets/bg.png" alt="背景图" width="100%">
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+
+  }
+}
+</script>
+
+<style scoped lang="less">
+
+</style>
---
+++ vue.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+	pages: {
+		main: {
+			entry: 'src/main/main.js',
+			template: 'public/index.html',
+			filename: 'main.html',
+			title: '广易'
+		},
+		redwar: {
+			entry: 'src/redwar/redwar.js',
+			template: 'public/index.html',
+			filename: 'redwar.html',
+			title: '福利红包信息'
+		},
+		project: {
+			entry:  'src/project/project.js',
+			template: 'public/index.html',
+			filename: 'project.html',
+			title: '项目信息'
+		}
+	}
+}
